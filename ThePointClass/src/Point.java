93d46ccc630057e71f05112ee480c5b7a28aba19
Secunda <SECUNJAC000@f109-12087.nicolet.us>
2018-03-21 11:16:15 -0500

    Work on Point Class

--- ThePointClass/src/Point.java
+++ ThePointClass/src/Point.java
@@ -53,4 +53,58 @@ public class Point {
 		
 		return -1;
 	}
+	
+	public Point midpoint(Point otherPoint)
+	{
+		Point a = this;
+		Point b = otherPoint;
+		
+		double midX = (a.getX() + b.getX()) / 2;
+		double midY = (a.getY() + b.getY()) / 2;
+		
+		return new Point(midX, midY);
+	}
+	
+	private double calcXDist(Point otherPoint)
+	{
+		return this.getX() - otherPoint.getX();
+	}
+	
+	private double calcYDist(Point otherPoint)
+	{
+		return this.getY() - otherPoint.getY();
+	}
+	
+	public double distance(Point otherPoint)
+	{	
+		double xDist = Math.abs(calcXDist(otherPoint));
+		double yDist = Math.abs(calcYDist(otherPoint));
+		
+		double distance = Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+				
+		return distance;
+	}
+	
+	public String equation(Point endLinePoint)
+	{		
+		if (endLinePoint.getX() == this.getX())
+		{
+			return "x = " + this.getX();
+		} else if (endLinePoint.getY() == this.getY()) {
+			return "y = " + this.getY();
+		}
+		
+		String finalEquation = "y = mx + b";
+		
+		double xDist = calcXDist(endLinePoint);
+		double yDist = calcYDist(endLinePoint);
+		
+		double interY = endLinePoint.getY() - ((yDist/xDist) * endLinePoint.getX());
+		String slope = xDist + "/" + yDist;
+		
+		finalEquation.replaceFirst("m", slope);
+		finalEquation.replaceFirst("b", Double.toString(interY));
+		
+		return finalEquation;
+	}
 }
---
+++ ThePointClass/src/PointTester.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class PointTester {
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Welcome to the Point Tester!");
+		System.out.println("Please enter a point by entering an x and a y:");
+	}
+}
