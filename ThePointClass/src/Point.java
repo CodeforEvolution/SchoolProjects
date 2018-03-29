1dc33e91aa08383e6b53e4058685055fbcd4bbc8
Jake <themysterymail555@gmail.com>
2018-03-28 22:10:43 -0500

    Almost Done PointClass

--- ThePointClass/src/Point.java
+++ ThePointClass/src/Point.java
@@ -1,35 +1,43 @@
+import java.lang.Math;
 
 public class Point {
 	private double x;
 	private double y;
-	
+
 	public Point()
 	{
 		x = 0.0;
 		y = 0.0;
 	}
-	
-	public Point(double theX, double theY)
+
+	public Point(double XDist, double YAngle, boolean isVector)
 	{
-		x = theX;
-		y = theY;
+        if (isVector == true)
+        {
+            x = XDist;
+            y = YAngle;
+        } else {
+            x = XDist * Math.acos(YAngle);
+            y = XDist * Math.asin(YAngle);
+
+        }
 	}
-	
+
 	public double getX()
 	{
 		return x;
 	}
-	
+
 	public double getY()
 	{
 		return y;
 	}
-	
+
 	public String toString()
 	{
 		return "(" + x + "," + y + ")";
 	}
-	
+
 	public int quadrant()
 	{
 		if (x == 0 || y == 0)
@@ -50,61 +58,92 @@ public class Point {
 				return 4;
 			}
 		}
-		
+
 		return -1;
 	}
-	
+
 	public Point midpoint(Point otherPoint)
 	{
 		Point a = this;
 		Point b = otherPoint;
-		
+
 		double midX = (a.getX() + b.getX()) / 2;
 		double midY = (a.getY() + b.getY()) / 2;
-		
-		return new Point(midX, midY);
+
+		return new Point(midX, midY, false);
 	}
-	
-	private double calcXDist(Point otherPoint)
+
+	double calcXDist(Point otherPoint)
 	{
 		return this.getX() - otherPoint.getX();
 	}
-	
-	private double calcYDist(Point otherPoint)
+
+	double calcYDist(Point otherPoint)
 	{
 		return this.getY() - otherPoint.getY();
 	}
-	
+
 	public double distance(Point otherPoint)
-	{	
+	{
 		double xDist = Math.abs(calcXDist(otherPoint));
 		double yDist = Math.abs(calcYDist(otherPoint));
-		
+
 		double distance = Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
-				
+
 		return distance;
 	}
-	
+
 	public String equation(Point endLinePoint)
-	{		
+	{
 		if (endLinePoint.getX() == this.getX())
 		{
 			return "x = " + this.getX();
 		} else if (endLinePoint.getY() == this.getY()) {
 			return "y = " + this.getY();
 		}
-		
+
 		String finalEquation = "y = mx + b";
-		
+
 		double xDist = calcXDist(endLinePoint);
 		double yDist = calcYDist(endLinePoint);
-		
+
 		double interY = endLinePoint.getY() - ((yDist/xDist) * endLinePoint.getX());
 		String slope = xDist + "/" + yDist;
-		
+
 		finalEquation.replaceFirst("m", slope);
 		finalEquation.replaceFirst("b", Double.toString(interY));
-		
+
 		return finalEquation;
+	}
+
+	public void toVector(double outDistance, double outAngle)
+	{
+		double x = getX();
+		double y = getY();
+
+		outDistance = distance(new Point(0,0,false));
+		outAngle = Math.sin(calcYDist(new Point(0,0,false)) / calcXDist(new Point(0,0,false)));
+
+        return;
+	}
+
+	public Point vectorToPoint(double distance, double angle)
+	{
+        double outX = distance * Math.acos(angle);
+        double outY = distance * Math.asin(angle);
+        
+		return new Point(outX, outY, false);
+	}
+
+	public void addVectors(double inDist, double inAngle, double outDist, double outAngle)
+	{
+        Point onePoint = this;
+        Point otherPoint = new Point(inDist, inAngle, true);
+        
+        Point addedPoint = new Point(onePoint.getX() + otherPoint.getX(), onePoint.getY() + otherPoint.getY(), false);
+        
+        addedPoint.toVector(outDist, outAngle);
+        
+		return;
 	}
 }
--- ThePointClass/src/PointTester.java
+++ ThePointClass/src/PointTester.java
@@ -1,12 +1,59 @@
 import java.util.Scanner;
+import java.util.Vector;
 
 
 public class PointTester {
 	public static void main(String[] args)
 	{
+		double theX = 0.0;
+		double theY = 0.0;
+		double otherX = 0.0;
+		double otherY = 0.0;
+
 		Scanner s = new Scanner(System.in);
-		
+
 		System.out.println("Welcome to the Point Tester!");
-		System.out.println("Please enter a point by entering an x and a y:");
+		System.out.println("Please enter a point by entering an x, than a y:");
+		theX = s.nextDouble();
+		theY = s.nextDouble();
+
+		Point thePoint = new Point(theX, theY, false);
+
+		System.out.println("Please enter another point by entering another x, than another y:");
+		otherX = s.nextDouble();
+		otherY = s.nextDouble();
+
+		Point otherPoint = new Point(otherX, otherY, false);
+        
+        double aDist = 0.0;
+        double aAng = 0.0;
+        double bDist = 0.0;
+        double bAng = 0.0;
+        
+        thePoint.toVector(aDist, aAng);
+        otherPoint.toVector(bDist, bAng);
+        
+		System.out.println("1st Point: " + thePoint);
+        System.out.println("Combined Vectors: Distance: " + aDist + " Angle: " + aAng);
+		System.out.println("2nd Point: " + otherPoint);
+        System.out.println("Combined Vectors: Distance: " + bDist + " Angle: " + bAng);
+
+		double xDist = thePoint.calcXDist(otherPoint);
+		double yDist = thePoint.calcXDist(otherPoint);
+		double lineDist = thePoint.distance(otherPoint);
+		String lineEquation = thePoint.equation(otherPoint);
+		Point theMid = thePoint.midpoint(otherPoint);
+        
+        double addDist = 0.0;
+        double addAng = 0.0;
+        
+        thePoint.addVectors(addDist, addAng, bDist, bAng);
+
+		System.out.println("Distance Between Point's Xs: " + xDist);
+		System.out.println("Distance Between Point's Ys: " + yDist);
+		System.out.println("Distance Between Points: " + lineDist);
+		System.out.println("Equation of the line: " + lineEquation);
+		System.out.println("Midpoint of the line: " + theMid);
+        System.out.println("Combined Vectors: Distance: " + addDist + " Angle: " + addAng);
 	}
 }
